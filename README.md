79b2f67a7abb0d19582d4a4c411b314f259d7605
Belati <belatijagadbintangsyuhada@gmail.com>
2024-11-22 23:11:57 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Eingrad
+A small project of mine, to satisfy my curiosity about the einstein summation feature in PyTorch. I'm planning to rewrite most of deep learning architecture with einsum if possible. Maybe I'll go a bit further when I'm finished, maybe not :)
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Exploring einsum'
+LONG_DESCRIPTION = 'Solely made to explore more about einstein summation operator.'
+
+setup(
+	name='eiengrad', 
+	version=VERSION,
+	author='Belati Jagad Bintang Syuhada',
+	author_email='belatijagadbintangsyuhada@gmail.com',
+	description=DESCRIPTION,
+	long_description=LONG_DESCRIPTION,
+	packages=find_packages(),
+	install_requires=[
+		'torch>=2.5',
+		'tqdm>=4.6',
+	], 
+)
