10daa81a3e2ece890378e38964aedc260496288b
Belati <belatijagadbintangsyuhada@gmail.com>
2024-11-23 14:46:53 +0700

    Implement multihead attention

--- eiengrad/models/transformer.py
+++ eiengrad/models/transformer.py
@@ -48,3 +48,34 @@ class ResidualConnection(nn.Module):
 		self.norm = LayerNorm(features)
 	def forward(self, x):
 		return x + self.dropout(self.norm(x))
+
+
+class MultiHeadAttention(nn.Module):
+  def __init__(self, d_model: int, num_heads: int, dropout_prob: float) -> None:
+    super().__init__()
+    assert d_model % num_heads == 0, "Embedding dimension must be divisible by num_heads"
+    self.d_model = d_model
+    self.num_heads = num_heads
+    self.d_k = d_model // num_heads
+    self.w_q = nn.Linear(d_model, d_model, bias=False)
+    self.w_k = nn.Linear(d_model, d_model, bias=False)
+    self.w_v = nn.Linear(d_model, d_model, bias=False)
+    self.w_o = nn.Linear(d_model, d_model, bias=False)
+    self.dropout = nn.Dropout(p=dropout_prob)
+  def forward(self, q, k, v, mask):
+    batch, seq_len, d_model = q.shape
+    # [batch, seq_len, d_model] -> [batch, seq_len, d_k, num_heads]
+    Q = self.w_q(q).view(batch, seq_len, self.d_k, self.num_heads)
+    K = self.w_k(k).view(batch, seq_len, self.d_k, self.num_heads)
+    V = self.w_v(v).view(batch, seq_len, self.d_k, self.num_heads)
+    # [batch, seq_len, d_k, num_heads] @ [batch, d_k, seq_len, num_heads] -> [batch, seq_len, seq_len, num_heads]
+    attention_logits = torch.einsum('bsdh,bdsh->bssh', [Q, K]) / math.sqrt(self.d_k)
+    if mask is not None:
+      attention_logits = attention_logits.masked_fill_(mask == 0, float('-inf'))
+    attention_logits = attention_logits.softmax(dim=-1)
+    self.attention = self.dropout(attention_scores)
+    # [batch, seq_len, seq_len, num_head] @ [batch, seq_len, d_k, num_head] -> [batch, seq_len, d_k, num_head]
+    attention_scores = torch.einsum('bssh,bsdh->bsdh', [self.attention, V])
+    # [batch, seq_len, d_k, num_head] -> [batch, seq_len, d_model]
+    attention_scores = attention_scores.view(batch, seq_len, d_model)
+    return self.w_o(attention_scores)
