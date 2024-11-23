8208586e549e979d04c05315be2f23bc171dd1a4
Belati <belatijagadbintangsyuhada@gmail.com>
2024-11-23 17:10:01 +0700

    Make tests for attention free transformer

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,6 +1,7 @@
 import torch
 import unittest
-from eiengrad.models.transformer import *
+from eiengrad.models.transformer.multi_head_attention import *
+from eiengrad.models.transformer.attention_free_transformer import *
 
 class TestMultiHeadAttention(unittest.TestCase):
   def setUp(self):
@@ -10,21 +11,44 @@ class TestMultiHeadAttention(unittest.TestCase):
     self.Ti = 48
     self.Tj = 32
     self.B = 2
+    self.mha = MultiHeadAttention(self.d_model, self.H, self.dropout_prob)
   def test_self_attention(self):
-    mha = MultiHeadAttention(self.d_model, self.H, self.dropout_prob)
     x = torch.rand(self.B, self.Ti, self.d_model)
-    output = mha(x, x, x)
+    output = self.mha(x, x, x)
     self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
   def test_masked_self_attention(self):
-    mha = MultiHeadAttention(self.d_model, self.H, self.dropout_prob)
     x = torch.rand(self.B, self.Ti, self.d_model)
     mask = torch.tril(torch.ones(self.Ti, self.Ti)).view(1, 1, self.Ti, self.Ti)
-    output = mha(x, x, x, mask)
+    output = self.mha(x, x, x, mask)
     self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
   def test_cross_attention(self):
-    mha = MultiHeadAttention(self.d_model, self.H, self.dropout_prob)
     x = torch.rand(self.B, self.Ti, self.d_model)
     y = torch.rand(self.B, self.Tj, self.d_model)
     mask = torch.tril(torch.ones(self.Ti, self.Tj)).view(1, 1, self.Ti, self.Tj)
-    output = mha(x, y, y, mask)
+    output = self.mha(x, y, y, mask)
+    self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
+    
+class TestAttentionFreeTransformer(unittest.TestCase):
+  def setUp(self):
+    self.d_model = 512
+    self.H = 8
+    self.dropout_prob = 0.1
+    self.Ti = 48
+    self.Tj = 32
+    self.B = 2
+    self.aft = AttentionFreeTransformer(self.d_model, self.dropout_prob)
+  def test_self_attention(self):
+    x = torch.rand(self.B, self.Ti, self.d_model)
+    output = self.aft(x, x, x)
+    self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
+  def test_masked_self_attention(self):
+    x = torch.rand(self.B, self.Ti, self.d_model)
+    mask = torch.tril(torch.ones(self.Ti, self.Ti)).view(1, 1, self.Ti, self.Ti)
+    output = self.aft(x, x, x, mask)
+    self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
+  def test_cross_attention(self):
+    x = torch.rand(self.B, self.Ti, self.d_model)
+    y = torch.rand(self.B, self.Tj, self.d_model)
+    mask = torch.tril(torch.ones(self.Ti, self.Tj)).view(1, 1, self.Ti, self.Tj)
+    output = self.aft(x, y, y, mask)
     self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
