5c0fa2f35cc29c5cbf8d57a2147b8b635774e4ee
Belati <belatijagadbintangsyuhada@gmail.com>
2024-11-23 23:13:38 +0700

    Add testcase for AFTSimple implementation

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -80,3 +80,30 @@ class TestAFTLocal(unittest.TestCase):
     mask = torch.tril(torch.ones(self.Ti, self.Tj, dtype=torch.bool)).view(1, self.Ti, self.Tj)
     output = self.aft(x, y, y, mask)
     self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
+    
+class TestAFTSimple(unittest.TestCase):
+  def setUp(self):
+    self.max_seqlen = 64
+    self.d_model = 512
+    self.d_hidden = 128
+    self.local_window_size = 0 # The only difference between AFTLocal and AFTSimple is that AFTSimple has s = 0
+    self.H = 8
+    self.Ti = 48
+    self.Tj = 32
+    self.B = 2
+    self.aft = AttentionFreeTransformer(d_model=self.d_model, max_seqlen=self.max_seqlen, d_hidden=self.d_hidden, local_window_size=self.local_window_size)
+  def test_self_attention(self):
+    x = torch.rand(self.B, self.Ti, self.d_model)
+    output = self.aft(x, x, x)
+    self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
+  def test_masked_self_attention(self):
+    x = torch.rand(self.B, self.Ti, self.d_model)
+    mask = torch.tril(torch.ones(self.Ti, self.Ti, dtype=torch.bool)).view(1, self.Ti, self.Ti)
+    output = self.aft(x, x, x, mask)
+    self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
+  def test_cross_attention(self):
+    x = torch.rand(self.B, self.Ti, self.d_model)
+    y = torch.rand(self.B, self.Tj, self.d_model)
+    mask = torch.tril(torch.ones(self.Ti, self.Tj, dtype=torch.bool)).view(1, self.Ti, self.Tj)
+    output = self.aft(x, y, y, mask)
+    self.assertEqual(output.shape, (self.B, self.Ti, self.d_model))
