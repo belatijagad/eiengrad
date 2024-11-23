f7f25d34198f3ee925dd994b47e3034b6fba692a
Belati <belatijagadbintangsyuhada@gmail.com>
2024-11-24 04:29:00 +0700

    [MTL] Implement dataset utility function

--- examples/mtl/train.py
+++ examples/mtl/train.py
@@ -36,3 +36,17 @@ def get_or_build_tokenizer(config, ds, lang):
   else: 
     tokenizer = Tokenizer.from_file(str(tokenizer_path))
   return tokenizer
+
+def get_dataset(config):
+  raw_dataset = load_dataset(config['datasource'], f"{config['lang_src']}-{config['lang_tgt']}", split='train')
+  tokenizer_src = get_or_build_tokenizer(config, raw_dataset, config['lang_src'])
+  tokenizer_tgt = get_or_build_tokenizer(config, raw_dataset, config['lang_tgt'])
+  train_ds_size = int(0.9*len(raw_dataset))
+  val_ds_size = len(raw_dataset) - train_ds_size
+  train_ds_raw, val_ds_raw = random_split(raw_dataset, [train_ds_size, val_ds_size])
+  train_dataset = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+  val_dataset   = BilingualDataset(val_ds_raw,   tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+  train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
+  val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True)
+  return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+  
