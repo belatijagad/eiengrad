a20e209359d1e6c5b6795e25a2099f3d4002c091
Belati <belatijagadbintangsyuhada@gmail.com>
2024-11-24 04:46:54 +0700

    [MTL] Implement training loop

--- examples/mtl/train.py
+++ examples/mtl/train.py
@@ -50,3 +50,28 @@ def get_dataset(config):
   val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=True)
   return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
   
+def train_model(config, model):
+  device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+  train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_dataset(config)
+  model = model.to(device)
+  optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'], eps=1e-9)
+  loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1).to(device)
+  for epoch in range(1, config['num_epochs']):
+    model.train()
+    batch_iterator = tqdm(train_dataloader, desc=f'Processing epoch: {epoch:02d}')
+    for batch in batch_iterator:
+      encoder_input = batch['encoder_input'].to(device)
+      decoder_input = batch['decoder_input'].to(device)
+      encoder_mask = batch['encoder_mask'].to(device)
+      decoder_mask = batch['decoder_mask'].to(device)
+      encoder_output = model.encode(encoder_input, encoder_mask)
+      decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask)
+      proj_output = model.project(decoder_output)
+      label = batch['label'].to(device)
+      loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
+      batch_iterator.set_postfix({'Loss': f'{loss.item():6.3f}'})
+      loss.backward()
+      optimizer.step()
+      optimizer.zero_grad(set_to_none=True)
+    run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg))
+    torch.save({'epoch': epoch, 'model_state_dict': model.state_dict(), 'optimizer_state_dict': optimizer.state_dict()}, config['model_name'])
