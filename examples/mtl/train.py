30c31c7ea384dcca87d27a1ed7be3bd59c773dac
Belati <belatijagadbintangsyuhada@gmail.com>
2024-11-24 02:05:30 +0700

    [MTL] Implement tokenizer builder

---
+++ examples/mtl/train.py
@@ -0,0 +1,38 @@
+"""
+  References:
+  - https://github.com/hkproj/pytorch-transformer/blob/main/train.py
+"""
+from dataset import BilingualDataset, causal_mask
+
+import torchtext.datasets as datasets
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim.lr_scheduler import LambdaLR
+
+import warnings
+from tqdm import tqdm
+import os
+from pathlib import Path
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+def get_all_sentences(ds, lang):
+  for item in ds:
+    yield item['translation'][lang]
+
+def get_or_build_tokenizer(config, ds, lang):
+  tokenizer_path = Path(config['tokenizer_file'].format(lang))
+  if not Path.exists(tokenizer_path):
+    tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+    tokenizer.pre_tokenizer = Whitespace()
+    trainer = WordLevelTrainer(special_tokens=['[UNK]', '[PAD]', '[SOS]', '[EOS]'], min_frequency=2)
+    tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+    tokenizer.save(str(tokenizer_path))
+  else: 
+    tokenizer = Tokenizer.from_file(str(tokenizer_path))
+  return tokenizer
